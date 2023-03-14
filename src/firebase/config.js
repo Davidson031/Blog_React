7ea1df03507c22a607a85383dc18c778e551806f
davidson <davidson031@hotmail.com>
2023-03-14 13:17:37 -0300

    componente de post individual criado e dados do post sendo passados por props

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  
+ 
 };
 
 // Initialize Firebase
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,11 +5,13 @@ import styles from "./Home.module.css";
 //hooks
 import { useNavigate, Link } from "react-router-dom";
 import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import PostDetail from "../../components/PostDetail";
 
 const Home = () => {
 
   const [query, setQuery] = useState("");
-  const [posts] = useState([]);
+  const { documents: posts, loading } = useFetchDocuments("posts");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -25,7 +27,10 @@ const Home = () => {
         </form>
 
         <div>
-          <h1>Posts...</h1>
+          { loading && <p>Carregando...</p>}
+          { posts && posts.map((post) => (
+            <PostDetail key={ post.id } post = { post }/>
+          ))}
           {(posts && posts.length === 0) &&(
             <div className= {styles.noposts}>
               <p>Não foram encontrados posts!</p>
