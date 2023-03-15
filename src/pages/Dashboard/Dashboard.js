dc8458bbdd7b957bccedcfc76b63268687cd692d
davidson <davidson031@hotmail.com>
2023-03-15 12:27:00 -0300

    estruturacao inicial da pagina de dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,28 +1,33 @@
 import styles from "./Dashboard.module.css";
 
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+//hooks
 import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 const Dashboard = () => {
 
-  const [title, setTitle] = useState("");
-  const [image, setImage] = useState("");
-  const [body, setBody] = useState("");
-  const [tags, setTags] = useState([]);
-  const [formError, setFormError] = useState("");
+  const { user } = useAuthValue();
 
+  const uid = user.uid;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-
-
-  }
+  const posts = [];
 
   return (
     <div>
-        
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      { (posts && posts.length === 0) ? (
+        <div className= { styles.noposts }>
+          <p>Não foram encontrados posts!</p>
+          <Link to= "/posts/create" className="btn">Criar primeiro post</Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts!</p>
+        </div>
+      ) }
     </div>
   )
 }
